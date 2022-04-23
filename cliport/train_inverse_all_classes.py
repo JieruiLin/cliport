2b2d74e6cfe658753f9f1f436423cff59aade1e8
JieruiLin <jerrylin0928@berkeley.edu>
2022-04-23 16:56:37 -0500

    add classifiers: action-only & action-with-objects

--- cliport/train_inverse_all_classes.py
+++ cliport/train_inverse_all_classes.py
@@ -11,13 +11,14 @@ from torch.nn import init
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from cliport import agents
-from cliport.dataset import ForwardDataset, ForwardDatasetClassification
+from cliport.dataset import ForwardDataset, ForwardDatasetClassification, ForwardDatasetClassificationAllObjects
 from cliport.utils import utils
 from cliport.environments.environment import Environment
 import wandb
 from tqdm import tqdm
 import clip
 import numpy as np
+import torchvision.models as models
 
 
 mode = 'train'
@@ -34,61 +35,32 @@ cfg['task'] = task
 cfg['mode'] = mode
 
 data_dir = os.path.join(root_dir, 'data')
-train_dataset = ForwardDatasetClassification(os.path.join(data_dir, f'{cfg["task"]}-train'), cfg, n_demos=500, augment=None)
+train_dataset = ForwardDatasetClassificationAllObjects(os.path.join(data_dir, f'{cfg["task"]}-train'), cfg, n_demos=500, augment=None)
 train_data_loader = DataLoader(train_dataset, batch_size=64)
-test_dataset = ForwardDatasetClassification(os.path.join(data_dir, f'{cfg["task"]}-val'), cfg, n_demos=100, augment=None)
+# use train set for val for now since not support other attributes
+test_dataset = ForwardDatasetClassificationAllObjects(os.path.join(data_dir, f'{cfg["task"]}-train'), cfg, n_demos=100, augment=None)
 test_data_loader = DataLoader(test_dataset, batch_size=64)
 
+all_languages = np.load("/home/jerrylin/temp/cliport/data/language_dictionary.npy")
+all_actions = np.load("/home/jerrylin/temp/cliport/data/action_dictionary.npy")
 
 class ICMModel(nn.Module):
     def __init__(self, use_cuda=True):
         super(ICMModel, self).__init__()
         self.device = torch.device('cuda' if use_cuda else 'cpu')
-        feature_output = 16 * 6 * 64
-        self.feature = nn.Sequential(
-            nn.Conv2d(
-                in_channels=3,
-                out_channels=32,
-                kernel_size=8,
-                stride=4),
-            nn.LeakyReLU(),
-            nn.Conv2d(
-                in_channels=32,
-                out_channels=64,
-                kernel_size=4,
-                stride=2),
-            nn.LeakyReLU(),
-            nn.Conv2d(
-                in_channels=64,
-                out_channels=64,
-                kernel_size=4,
-                stride=2),
-            nn.LeakyReLU(),
-            nn.Conv2d(
-                in_channels=64,
-                out_channels=64,
-                kernel_size=3,
-                stride=1),
-            nn.LeakyReLU(),
-            nn.Flatten(),
-            nn.Linear(feature_output, 512)
-        )
-        
+
+        self.feature = models.resnet18()
+        self.feature.fc = nn.Linear(512, 512)
+
         self.inverse_net = nn.Sequential(
             nn.Linear(512 * 2, 512),
-            nn.ReLU(),
-            nn.Linear(512, 3)
+            nn.LeakyReLU(),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(),
+            nn.Linear(256, 256),
+            nn.LeakyReLU(),
+            nn.Linear(256, len(all_languages))
         )
-
-
-        for p in self.modules():
-            if isinstance(p, nn.Conv2d):
-                init.kaiming_uniform_(p.weight)
-                p.bias.data.zero_()
-
-            if isinstance(p, nn.Linear):
-                init.kaiming_uniform_(p.weight, a=1.0)
-                p.bias.data.zero_()
 
     def forward(self, state, next_state):
         encode_state = self.feature(state[:,:3])
@@ -107,13 +79,12 @@ ce = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=1e-4)
 wandb.init(project='forward_inverse_model')
 
-all_languages = ['pack', 'stack', 'put'] #np.load("/home/jerrylin/temp/cliport/data/language_dictionary.npy")
-all_actions = np.load("/home/jerrylin/temp/cliport/data/action_dictionary.npy")
-
 def evaluate_inverse_model(ds, model):
     pred_languages_all_episode = []
     total = 0
     correct = 0
+    model.eval()
+
     for i in range(10):
         episode_id = np.random.choice(ds.sample_set)
         episode, seed = ds.load(episode_id)
@@ -124,24 +95,20 @@ def evaluate_inverse_model(ds, model):
             if reward > 0:
                 state = ds.process_sample(episode[step], augment=None)
                 next_state = ds.process_sample(episode[step + 1], augment=None)
-                image = torch.from_numpy(state['img'])[None,:,:,:3]
-                next_image = torch.from_numpy(next_state['img'])[None,:,:,:3]
+                image = torch.from_numpy(state['img'])[None]
+                next_image = torch.from_numpy(next_state['img'])[None]
                 image = image.cuda().float().permute(0,3,1,2)/255.
                 next_image = next_image.cuda().float().permute(0,3,1,2)/255.
                 # ground-truth lang goal
-                lang_goal = state['lang_goal'].split()
-                if lang_goal[0] == 'pack':
-                    cls = 0
-                elif lang_goal[0] == 'stack':
-                    cls = 1
-                else:
-                    cls = 2
+                lang_goal = state['lang_goal']
+                cls = np.where(all_languages == lang_goal)[0]
                 with torch.no_grad():
                     pred_action = model(image, next_image)
-                    
+
                 predicted = torch.argmax(pred_action, 1)
                 total += 1
-                correct += (predicted == cls).float()
+
+                correct += float(predicted == cls)
 
                 pred_language = all_languages[predicted]
                 pred_languages.append(pred_language)
@@ -157,15 +124,15 @@ def train_or_val(flag, data_loader):
 
     total = 0
     correct = 0
-
     for batch_idx, sample in enumerate(tqdm(data_loader)):
         state, next_state, action = sample
         state = state.cuda().float().permute(0,3,1,2)/255.
         next_state = next_state.cuda().float().permute(0,3,1,2)/255.
         action = action.long().cuda()
+
         pred_action = model(state, next_state)
         inverse_loss = ce(pred_action, action)
-
+        
         predicted = torch.argmax(pred_action, 1)
         total += pred_action.shape[0]
         correct += (predicted == action).float().sum()
@@ -174,7 +141,7 @@ def train_or_val(flag, data_loader):
             loss = inverse_loss
             optimizer.zero_grad()
             loss.backward()
-            #torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+            #torch.nn.utils.clip_grad_norm_(model.parameters(), 0.1)
             optimizer.step()
     return inverse_loss, correct/total
 
@@ -188,14 +155,15 @@ if TRAIN:
         inverse_loss_val, accuracy_val = train_or_val('val', test_data_loader)
         pred_languages, accuracy_test = evaluate_inverse_model(train_dataset, model)
         print(pred_languages)
+        
         wandb.log({"inverse_loss_train": inverse_loss_train.item(),
                    "inverse_loss_val": inverse_loss_val.item(),
                    "accuracy_train": accuracy_train.item(),
                    "accuracy_val": accuracy_val.item(),
-                   "accuracy_test": accuracy_test.item()})
-
+                   "accuracy_test": accuracy_test})
+        
         if epoch % 10 == 0:
-            torch.save(model, "icm_model_action_only_three_classes.pt")
+            torch.save(model, "icm_model_all_classes_with_object_classes.pt")
 else:
     pred_languages, accuracy = evaluate_inverse_model(train_dataset, model)
     print(accuracy)
